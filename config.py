89d55bd1d828d3618a24bf38e6186b623f868a1a
Aditya Ghosh <adityaghosh5025@gmail.com>
2022-05-31 00:01:22 +0530

    Added file

---
+++ config.py
@@ -0,0 +1,33 @@
+# Default values
+
+
+# Passphrase options
+WORD_COUNT = 4              # Number of words in passphrase
+SEPARATOR = '-'             # Passphrase separator
+IS_CAPITAL = False          # Whether first letters are capitalized
+
+# Password Options
+PASSWORD_LENGTH = 20        # Number of characters in password
+NO_UPPER = True             # Skip uppercase characters
+NO_LOWER = True             # Skip lowercase characters
+NO_DIGIT = True             # Skip digits
+NO_SPECIAL = False          # Skip special characters
+
+FILES_PATH = "files/"       # File Paths
+
+# List of Words to use
+WORDS_FILE = "words.txt"
+# Uncomment the line below to use a much larger of collection of words for passphrase generation
+# Howver Note that most of the words in this file are either uncommon or sometimes acronyms
+#WORDS_FILE = "words_big.txt"
+
+
+
+# Dont change anything below
+
+# Print "[DEFAULT: On/Off]" in help text
+default_is_capital = "On" if IS_CAPITAL else "Off"
+default_no_upper = "On" if NO_UPPER else "Off"
+default_no_lower = "On" if NO_LOWER else "Off"
+default_no_digit = "On" if NO_DIGIT else "Off"
+default_allow_special = "On" if NO_SPECIAL else "Off"
---
+++ pass_generators.py
@@ -0,0 +1,62 @@
+import random
+
+def gen_upper():
+    '''Generates a Random Uppercase Letter'''
+    return chr(random.randint(65, 90))
+            
+def gen_lower():
+    '''Generates a Random Lowercase Letter'''
+    return chr(random.randint(97, 122))
+
+def gen_digit():
+    '''Generates a Random Digit'''
+    return chr(random.randint(48, 57))
+
+def gen_special():
+    '''Generates a Random Special Letter'''
+    return chr(random.randint(33, 47))
+
+
+
+def gen_password(length, allow_upper, allow_lower, allow_digit, allow_special):
+    """Generates a random password"""
+    password = ""
+    while len(password) < length:
+        choice = random.randint(1, 4)
+
+        if choice == 1 and allow_upper:
+            password += gen_upper()
+        elif choice == 2 and allow_lower:
+            password += gen_lower()
+        elif choice == 3 and allow_digit:
+            password += gen_digit()
+        elif choice == 4 and allow_special:
+            password += gen_special()
+    return password
+
+
+
+def gen_passphrase(filepath, word_count, separator, is_capital):
+    '''Generates a list of random words from a file and then performs the
+    required operations (adding capitalization and/or separator)'''
+    
+    with open(filepath, 'r') as words_file:
+        words_list = words_file.readlines()
+
+    # Generating random words
+    passphrase_words = []
+    while len(passphrase_words) < word_count:
+        num = random.randint(0, (len(words_list) - 1))
+        passphrase_words.append(words_list[num].strip())
+
+    # Capitalizing first letter of each word is specified
+    # Also makes all words starting with a capital letter lowercase 
+    # if no capitalize option specified
+    for i, word in enumerate(passphrase_words):
+        if word[0][0].isupper() and is_capital is False:
+            passphrase_words[i] = word.lower()
+        elif is_capital is True:
+            passphrase_words[i] = word.capitalize() 
+    
+    passphrase = separator.join(passphrase_words)
+    return passphrase
